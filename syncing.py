d846069dc5cc3d4d055b99b5d452ab0baba0f781
Andorrrrr <andorrturtle@gmail.com>
2023-05-13 12:24:02 +0200

    l
    l

---
+++ syncing.py
@@ -0,0 +1,51 @@
+import syncedlyrics
+#-------------------------------------------------------
+name = "i see london i see france"
+artist = "bbno$"
+#-------------------------------------------------------
+
+link = "not found"
+
+lrc = syncedlyrics.search(name + " " + artist)
+
+#print(lrc)
+
+lrc = lrc.replace('[', '"')
+lrc = lrc.replace(']', '": "')
+lrc = lrc.replace('\n', '",\n')
+lrc = lrc.replace('00:0', '')
+lrc = lrc.replace('00:', '')
+lrc = lrc.replace('01:', '1:')
+lrc = lrc.replace('02:', '2:')
+lrc = lrc.replace('03:', '3:')
+lrc = lrc.replace('.', ':')
+lrc = lrc.replace('" ', '"')
+lrc = lrc + '(The end)",'
+
+music_link = link
+lol = ", {"
+name = f'                          name: "{name + " - " + artist}"'
+src = f'                          src: "{music_link}",'
+sync = '                          sync: {'
+last = '                         },'
+last_2 = '                      }'
+
+whole = f'''
+{lol}
+{name}
+{src}
+{sync}
+{lrc}
+{last}
+{last_2}
+'''
+
+print(whole)
+#if lrc[6] == ":":
+    #print(lrc[0] + lrc[1] + lrc[2] + lrc[3]+  lrc[4] + lrc[5] + lrc[6])
+#elif lrc[7] == ":":
+    #print(lrc[0] + lrc[1] + lrc[2] + lrc[3]+  lrc[4] + lrc[5] + lrc[6] + lrc[7])
+#elif lrc[8] == ":":
+    #print(lrc[0] + lrc[1] + lrc[2] + lrc[3]+  lrc[4] + lrc[5] + lrc[6] + lrc[7] + lrc[8])
+#elif lrc[9] == ":":
+    #print(lrc[0] + lrc[1] + lrc[2] + lrc[3]+  lrc[4] + lrc[5] + lrc[6] + lrc[7] + lrc[8] + lrc[9])
